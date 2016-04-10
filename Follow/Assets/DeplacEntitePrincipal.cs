9aaaace1460eae7388e0e546f1c1e9fa14455837
Ishiwawa <arnaud_lecomte@outlook.com>
2016-04-10 12:17:48 +0200

    Déplacement bille en fonction de la souris
    La camera est très reculée, il faut voir comment la gérer. La caméra ne
    suit pas quand la bille sort de l'écran. La vitesse de la bille est très
    élevée, il faut voir comment la gérer. La bille peut sortir par le haut.
    La bille ne suit pas précisément la souris. Il faudrait adapter la
    vitesse en fonction de la distance de la souris.

---
+++ Follow/Assets/DeplacEntitePrincipal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeplacEntitePrincipal : MonoBehaviour {
+    
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    //Création du vecteur mouvement en dehors de l'update pour éviter de réinitialiser la position.
+    Vector3 move = new Vector3(0, 0, 0);
+
+    // Update is called once per frame
+    void Update () {
+
+       
+        //création d'un vecteur qui récupere la position de la souris
+        Vector3 mousePos = Input.mousePosition;
+       
+        //la bille se deplace dans une direction si elle se situe dans la quart extreme de la direction souhaité
+        //On associe les nouvelles positions à l'objet
+        transform.position = move;
+    }
+}
