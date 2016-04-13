8dfe8a89ab954868881604b45ff98525d55c43a2
Roikomilmi <roikomilmi@gmail.com>
2016-04-13 03:41:39 +0200

    Déplacement souris
    Ajout d'un script FollowMouse2D afin que l'entité joueur suive le
    curseur.

---
+++ Follow/Assets/FollowMouse2D.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class FollowMouse2D : MonoBehaviour {
+
+	private Vector3 mousePosition;
+	public float moveSpeed = 1f;
+
+  void Update()
+  {
+		Vector3 temp = Input.mousePosition;
+		temp.z = 10f; // Set this to be the distance you want the object to be placed in front of the camera.
+		this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+  }
+}
